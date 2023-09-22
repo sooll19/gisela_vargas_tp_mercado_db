f4a7e40451755f49a262d1c58c3da03e00fc2a2d
unknown <solvargas094@gmail.com>
2023-09-21 21:48:36 -0300

    termine desafio

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -1,5 +1,6 @@
 /*base de datos*/
 const db = require('../database/models')
+const { Op } = require('sequelize')
 
 
 const fs = require('fs');
@@ -12,21 +13,56 @@ const toThousand = n => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
 
 const controller = {
 	index: (req, res) => {
-	return res.render('index',{
-		visited: products.filter(product => product.category=== "visited"),
-		inSale: products.filter(product => product.category=== "in-sale"),
-		toThousand
+	
+	const visited =
+	db.Product.findAll({
+		where : {
+			categoryId :1
+		}
 	})
+	const sale = 
+	db.Product.findAll({
+		where:{
+			categoryId :2
+		}
+	})
+    Promise.all([visited,sale])
+    .then(([visited,sale]) => {
+        return res.render('index',{
+            visited,
+            sale,
+            toThousand
+        })
+    }).catch((error) => console.log(error))
 	},
 	search: (req, res) => {
-		const keywords = req.query.keywords
-		const results = products.filter(product => product.name.toLowerCase().includes(keywords.toLowerCase()))
-		return res.render('results',{
-			results,
-			toThousand,
-			keywords
-		})
-	},
-};
 
+        const keywords = req.query.keywords.toLowerCase();
+        db.Product.findAll({
+            where: {
+                [Op.or] : [
+                    {
+                        name: {
+                            [Op.substring] : keywords
+                        }
+                    },
+                    {
+                        description: {
+                            [Op.substring] : keywords
+                        }
+                    }
+                ]
+
+            }
+        })
+            .then(results => {
+                return res.render('results', {
+                    results,
+                    toThousand,
+                    keywords
+                });
+            })
+            .catch(error => console.log(error));
+		}
+	}
 module.exports = controller;
